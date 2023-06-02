64ea5e933379758608cd6b9e592e97f62df35f64
hboppe <hanna.nascimento31@gmail.com>
2023-06-02 14:34:45 -0400

    feat: start project and add app teams

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kopa.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kopa_do_mundo.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- utils.py
+++ utils.py
@@ -2,34 +2,33 @@ from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitle
 
 from datetime import datetime
 
+
 def data_processing(team_info: dict):
-  current_year = datetime.now().year
+    current_year = datetime.now().year
 
-  def get_first_cup_year():
-    first_cup_date = datetime.strptime(team_info['first_cup'], "%Y-%m-%d")
-    first_cup_year = first_cup_date.year
+    def get_first_cup_year():
+        first_cup_date = datetime.strptime(team_info["first_cup"], "%Y-%m-%d")
+        first_cup_year = first_cup_date.year
 
-    return first_cup_year
+        return first_cup_year
 
-  def check_first_year_cup():
-    first_cup_date = datetime.strptime(team_info['first_cup'], "%Y-%m-%d")
-    first_cup_year = first_cup_date.year
-    years_difference = first_cup_year - 1930
+    def check_first_year_cup():
+        first_cup_date = datetime.strptime(team_info["first_cup"], "%Y-%m-%d")
+        first_cup_year = first_cup_date.year
+        years_difference = first_cup_year - 1930
 
-    return years_difference
-  
-  first_cup_year = get_first_cup_year()
-  cup_years_difference = check_first_year_cup()
+        return years_difference
 
+    first_cup_year = get_first_cup_year()
+    cup_years_difference = check_first_year_cup()
 
-  if team_info['titles'] < 0:
-    raise NegativeTitlesError()
-  
-  if cup_years_difference % 4 != 0 or cup_years_difference < 0:
-    raise InvalidYearCupError()
-  
-  number_of_cups = (current_year - first_cup_year) // 4
+    if team_info["titles"] < 0:
+        raise NegativeTitlesError()
 
-  if number_of_cups < team_info['titles']:
-    raise ImpossibleTitlesError()
+    if cup_years_difference % 4 != 0 or cup_years_difference < 0:
+        raise InvalidYearCupError()
 
+    number_of_cups = (current_year - first_cup_year) // 4
+
+    if number_of_cups < team_info["titles"]:
+        raise ImpossibleTitlesError()
