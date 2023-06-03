f7d70938e0ce55adf492b31733ab31832379ff87
hboppe <hanna.nascimento31@gmail.com>
2023-06-03 12:48:36 -0400

    feat: add TeamDetailView with get, patch and delete

--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView, status
-
 from exceptions import ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
 from .models import Team
 from django.forms.models import model_to_dict
@@ -28,4 +27,45 @@ class TeamView(APIView):
     team = Team.objects.create(**request.data)
 
     return Response(model_to_dict(team), status.HTTP_201_CREATED)
+  
+
+class TeamDetailView(APIView):
+
+  def get_team(self, team_id: int) -> Response:
+    try:
+      return Team.objects.get(id=team_id)
+    except Team.DoesNotExist:
+      return Response({'message': 'Team not found'}, status.HTTP_404_NOT_FOUND)
+
+  def get(self, request: Request, team_id: int) -> Response:
+  
+    team = self.get_team(team_id)
+    if isinstance(team, Response):
+      return team
+    
+    return Response(model_to_dict(team), status.HTTP_200_OK)
+  
+  def patch(self, request: Request, team_id: int):
+    
+    team = self.get_team(team_id)
+    if isinstance(team, Response):
+      return team
+    
+    for key, value in request.data.items():
+      if key != 'id':
+        setattr(team, key, value)
+
+    team.save()
+
+    return Response(model_to_dict(team))
+
+  def delete(self, request: Request, team_id: int):
+    team = self.get_team(team_id)
+
+    if isinstance(team, Response):
+      return team
+    
+    team.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 
